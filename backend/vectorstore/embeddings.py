885ece1297e9686a5a855a4fc86093b7935e6557
620593 <scs620593@gmail.com>
2026-05-16 16:25:10 +0530

    fixed hugging face issue

--- backend/vectorstore/embeddings.py
+++ backend/vectorstore/embeddings.py
@@ -4,6 +4,6 @@ from backend.config import settings
 def get_embeddings() -> HuggingFaceEndpointEmbeddings:
     """Initializes and returns the HuggingFace embedding model via API."""
     return HuggingFaceEndpointEmbeddings(
-        model=f"https://api-inference.huggingface.co/pipeline/feature-extraction/{settings.embedding_model_name}",
+        model=settings.embedding_model_name,
         huggingfacehub_api_token=settings.hf_token,
     )
